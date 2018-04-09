6d4e4b1f654d73851352284f0bc04d3631c654b8
mauricio <Mauricio@Mauricios-MacBook-Pro.local>
2018-04-08 21:54:28 -0500

    Changing measure of how mucho rear view controller is showing

--- SlideView/Controllers/RearViewController.swift
+++ SlideView/Controllers/RearViewController.swift
@@ -12,24 +12,12 @@ class RearViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Establishing measure of how much the rear view controller is showing
+        //Animation duration can be modified in SWRevealViewController.m in line 659 changing toggleAnimationDuration value
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
 
-        // Do any additional setup after loading the view.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
